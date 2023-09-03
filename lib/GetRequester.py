06690a46558fe9a7c4e05a04d6c1312cef80fcfb
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-09-02 19:45:34 -0700

    lab complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response= requests.get(self.url)
+
+        return response.content
 
     def load_json(self):
-        pass
+
+        response_body = self.get_response_body()
+        response_text = response_body.decode('utf-8')  # Decode the bytes to a UTF-8 string
+        URL_data = json.loads(response_text)  # Parse JSON data from the response text
+        return URL_data
+    
